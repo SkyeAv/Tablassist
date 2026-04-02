6adfbab12bfa0bcd96368988897415af749c2dfc
SkyeAv <skye.lane.goetz@gmail.com>
2026-04-01 18:23:53 -0700

    Added Random Production Examples

--- cli/src/tablassist/cli.py
+++ cli/src/tablassist/cli.py
@@ -1,3 +1,5 @@
+import random
+
 from cyclopts import App
 from tablassert.enums import Categories, Predicates, Qualifiers
 
@@ -21,6 +23,19 @@ def get_advanced_table_configuration_examples_documentation() -> str:
 @CLI.command
 def get_tablassert_cli_tutorial_documentation() -> str:
     url: str = "https://raw.githubusercontent.com/SkyeAv/Tablassert/blob/main/docs/tutorial.md"
+    return get_static_content(url)
+
+
+@CLI.command
+def get_random_production_table_configuration_example() -> str:
+    urls: list[str] = [
+        "https://raw.githubusercontent.com/glusman-team/MOKGConfiguration/refs/heads/master/TABLE/MBKG/ALAM1.yaml",
+        "https://raw.githubusercontent.com/glusman-team/MOKGConfiguration/refs/heads/master/TABLE/MBKG/AVUTHU1.yaml",
+        "https://raw.githubusercontent.com/glusman-team/MOKGConfiguration/refs/heads/master/TABLE/MBKG/AVUTHU2.yaml",
+        "https://raw.githubusercontent.com/glusman-team/MOKGConfiguration/refs/heads/master/TABLE/MBKG/BLANTON1.yaml",
+        "https://raw.githubusercontent.com/glusman-team/MOKGConfiguration/refs/heads/master/TABLE/MBKG/BOHMAN4.yaml",
+    ]
+    url: str = urls[random.randint(0, 4)]
     return get_static_content(url)
 
 
